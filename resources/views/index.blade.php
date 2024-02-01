728c0dc5a0079a0b994b1f8d69df3471d0183ae4
mariomulyana <mariomulyana407@gmail.com>
2024-02-01 15:44:05 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/build.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hurricane&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <nav>
+        <div class="shadow-lg mx-auto">
+            <div class="  flex justify-between p-4 max-w-screen-md mx-auto">
+                <div class="font-fontutama text-3xl ">Galeri Saya</div>
+                <input type="text" class="bg-green-400 border-black px-4 py-1 mr-4 rounded-full"placeholder="Search........................">
+                <div class="flex gap-4 mt-2">
+                    <button class="bg-blue-800 px-4 py-1 text-white rounded-full">
+                    <a href="/login">Login</a></button>
+                    <button class="bg-blue-800 px-4 py-1 text-white rounded-full">
+                      <a href="/Register">Register</a></button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="mt-10">
+        <h1 class="text-center font-fountcontoh text-4xl">Kamu Memang Sudah Pergi</h1>
+        <h1 class="text-center font-fountcontoh text-3xl">Tapi Foto Mu Mesih Ada Di Galeri</h1>
+        <h1 class="text-center font-fountcontoh text-3xl">Anjay Kelazzzzz</h1>
+    </div>
+    <section class="flex justify-center gap-7 mt-6">
+        <div class="flex flex-col w-80 gap-5">
+            <img class="mb-3" src="/img/WhatsApp Image 2024-01-27 at 10.06.42.jpeg">
+            <img src="/img/image 5.png">
+        </div>
+
+        <div class="flex flex-row">
+            <img class="w-56" src="/img/image 3.png">
+        </div>
+    </section>
+</body>
+
+</html>
